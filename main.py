2fbbdb135e1bb15199ec1edb8ed013fc76f5d2fd
rizkyy2818 <beyemuhamad5@gmail.com>
2026-08-28 10:06:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class View(discord.ui.View):
         super().__init__()
         self.add_item(discord.ui.Button(
             label="🎮 Choose Room Now",
-            url="https://privateroomcondo.netlify.app"
+            url="https://privateroomscondo.netlify.app/"
         ))
 
 @bot.event
